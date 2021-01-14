0008938e624cb0871e7bb40bc9b918ca016b12d9
sdaundka <sagardaundkar007@gmail.com>
2021-01-14 10:40:07 +0530

    limit-service configuraration from application.properties using ConfigurationProperties

---
+++ currency-limit-service/src/main/java/com/currencylimitservice/LimitServicePropertyConfiguration.java
@@ -0,0 +1,26 @@
+package com.currencylimitservice;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(value = "limit-service")
+public class LimitServicePropertyConfiguration {
+	
+	private int minimum;
+	private int maximum;
+	
+	public int getMinimum() {
+		return minimum;
+	}
+	public void setMinimum(int minimum) {
+		this.minimum = minimum;
+	}
+	public int getMaximum() {
+		return maximum;
+	}
+	public void setMaximum(int maximum) {
+		this.maximum = maximum;
+	}
+
+}
--- currency-limit-service/src/main/java/com/currencylimitservice/resource/LimitConfigurationController.java
+++ currency-limit-service/src/main/java/com/currencylimitservice/resource/LimitConfigurationController.java
@@ -1,18 +1,30 @@
 package com.currencylimitservice.resource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.currencylimitservice.LimitServicePropertyConfiguration;
 import com.currencylimitservice.entities.LimitConfiguration;
 
 @RestController
-@RequestMapping("limit-service")
+@RequestMapping("/limit-service")
 public class LimitConfigurationController {
+	
+	@Autowired
+	private LimitServicePropertyConfiguration limitServicePropertyConfiguration;
 
-	@GetMapping
+	@GetMapping("/hardcoded")
 	public LimitConfiguration getLimits() {
+		//Create the hard coded object for the LimitConfiguration
 		return new LimitConfiguration(1, 999);
+	}
+	
+	@GetMapping("/appproperties")
+	public LimitConfiguration getLimitsFromApplicationProperties() {
+		//Create the hard coded object for the LimitConfiguration
+		return new LimitConfiguration(limitServicePropertyConfiguration.getMinimum(), limitServicePropertyConfiguration.getMaximum());
 	}
 	
 }
