dff296000705ef291c45fa220e91bbeaa59ab653
sdaundka <sagardaundkar007@gmail.com>
2021-01-16 13:01:41 +0530

    Get the properties from spring_cloud_config_server

--- currency-limit-service/src/main/java/com/currencylimitservice/LimitServicePropertyConfiguration.java
+++ currency-limit-service/src/main/java/com/currencylimitservice/LimitServicePropertyConfiguration.java
@@ -1,9 +1,10 @@
 package com.currencylimitservice;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
-@Configuration
+@Component
+//The value in ConfigurationProperties indicates the prefix of the property in properties file
 @ConfigurationProperties(value = "limit-service")
 public class LimitServicePropertyConfiguration {
 	
--- currency-limit-service/src/main/java/com/currencylimitservice/resource/LimitConfigurationController.java
+++ currency-limit-service/src/main/java/com/currencylimitservice/resource/LimitConfigurationController.java
@@ -9,19 +9,20 @@ import com.currencylimitservice.LimitServicePropertyConfiguration;
 import com.currencylimitservice.entities.LimitConfiguration;
 
 @RestController
-@RequestMapping("/limit-service")
+@RequestMapping("/limits")
 public class LimitConfigurationController {
 	
 	@Autowired
 	private LimitServicePropertyConfiguration limitServicePropertyConfiguration;
 
+	/*
 	@GetMapping("/hardcoded")
 	public LimitConfiguration getLimits() {
 		//Create the hard coded object for the LimitConfiguration
 		return new LimitConfiguration(1, 999);
-	}
+	}*/
 	
-	@GetMapping("/appproperties")
+	@GetMapping
 	public LimitConfiguration getLimitsFromApplicationProperties() {
 		//Create the hard coded object for the LimitConfiguration
 		return new LimitConfiguration(limitServicePropertyConfiguration.getMinimum(), limitServicePropertyConfiguration.getMaximum());
